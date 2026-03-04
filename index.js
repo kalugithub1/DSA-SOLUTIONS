ada56e18b8fd2fcbe7417a49dac34126bc98da66
kalugithub1 <167551008+kalugithub1@users.noreply.github.com>
2026-03-04 12:59:15 +0300

    Added a fuction to check if a number is a prime nuumber

--- index.js
+++ index.js
@@ -32,3 +32,21 @@ console.log(factorial(1)); // 1
 console.log(factorial(5)); //120
 
 //Big-O =O(n)
+
+// Given a natural number 'n' determine if the number is prime or not
+function isPrime(n) {
+  if (n < 2) {
+    return false;
+  }
+  for (let i = 2; i < n; i++) {
+    if (n % 2 === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log("isPrime Results:");
+console.log(isPrime(1)); //false
+console.log(isPrime(5)); //true
+console.log(isPrime(4)); //false
