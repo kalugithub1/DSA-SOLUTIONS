8a29e65762542906a979b9245d2dfb991d423682
kalugithub1 <167551008+kalugithub1@users.noreply.github.com>
2026-03-04 13:18:24 +0300

    isPrime function optimized& Big-O determined

--- index.js
+++ index.js
@@ -50,3 +50,26 @@ console.log("isPrime Results:");
 console.log(isPrime(1)); //false
 console.log(isPrime(5)); //true
 console.log(isPrime(4)); //false
+
+//Big-O =O(n)
+
+//isPrime Fuction Optimized
+
+function isPrime(n) {
+  if (n < 2) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % 2 === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log("isPrime Results:");
+console.log(isPrime(1)); //false
+console.log(isPrime(5)); //true
+console.log(isPrime(4)); //false
+
+//Big-O =O(sqrt(n))
