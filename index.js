5dc7b141291739f92b183ae9365dd435b580a3ba
kalugithub1 <167551008+kalugithub1@users.noreply.github.com>
2026-03-04 12:30:12 +0300

    Fibonacci & Factorial Calculated

---
+++ index.js
@@ -0,0 +1,34 @@
+//Given a number 'n', find the first elements of the Fibonacci sequence
+
+function fibonacci(n) {
+  const fib = [0, 1];
+
+  for (let i = 2; i < n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib;
+}
+
+console.log("Fibonacci Results:");
+console.log(fibonacci(2));
+console.log(fibonacci(3));
+console.log(fibonacci(7));
+console.log(fibonacci(20));
+
+//Big-O =O(n)
+
+// Given an integer 'n', find the factorial of that integer
+function factorial(n) {
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+console.log("Factorial Results:");
+console.log(factorial(0)); // 1
+console.log(factorial(1)); // 1
+console.log(factorial(5)); //120
+
+//Big-O =O(n)
