6eef3834bd2dc76abaf3e9a2a7daa51af7a63528
kalugithub1 <167551008+kalugithub1@users.noreply.github.com>
2026-03-04 13:48:05 +0300

    Added a fuction to check if a number is a power of two

--- index.js
+++ index.js
@@ -73,3 +73,25 @@ console.log(isPrime(5)); //true
 console.log(isPrime(4)); //false
 
 //Big-O =O(sqrt(n))
+
+// Given a positive integer 'n', determine if the number is a power of 2 or not
+
+function isPowerOfTwo(n) {
+  if (n < 1) {
+    return false;
+  }
+  while (n > 1) {
+    if (n % 2 !== 0) {
+      return false;
+    }
+    n = n / 2;
+  }
+  return true;
+}
+
+console.log("isPowerOfTwo Results:");
+console.log(isPowerOfTwo(1)); //true
+console.log(isPowerOfTwo(2)); //true
+console.log(isPowerOfTwo(5)); //false
+
+//Big-O = O(logn)
