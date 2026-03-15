c5b274d40bc68c3b27a553c682f9642489b42fae
kalugithub1 <167551008+kalugithub1@users.noreply.github.com>
2026-03-15 12:36:19 +0300

    BubbleSort and InsertionSort added

---
+++ sort.js
@@ -0,0 +1,37 @@
+function bubbleSort(arr) {
+  let swapped;
+
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+}
+
+const arr = [8, 20, -2, 4, -6];
+bubbleSort(arr);
+console.log(arr); //[-6,-2,4,8,20]
+
+// Big -O = O(n^2)
+
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let numberToInsert = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > numberToInsert) {
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+    arr[j + 1] = numberToInsert;
+  }
+}
+
+insertionSort(arr);
+console.log(arr); //[-6,-2,4,8,20]
+// Big -O = O(n^2)
